cddd5bdf5ee700d6498a0e4a163b706d4cab233e
Benny96 <benatgalaizp@gmail.com>
2017-12-19 17:28:03 +0100

    Validando HTML

--- js/agendactrl.js
+++ js/agendactrl.js
@@ -1,9 +1,5 @@
 app.controller("agendaCtrl", function ($scope) {
-/*
-  $scope.contactos = [
-    { name: 'Pablo Garaizar', email: 'garaizar@deusto.es', phone: '2512' }
-  ];
-*/
+
     $scope.contactos = [];
     $scope.query = "";
     $scope.nuevo = { name: '', email: '', phone: '' };
@@ -17,15 +13,14 @@ app.controller("agendaCtrl", function ($scope) {
         var clave = snapshot.key();
         // Añadir al array local
         $scope.contactos.push( { id: clave, name: contacto.name, email: contacto.email, phone: contacto.phone } );
-    });
+    //generarJson();
+  });
 
   $scope.refrescarContactos = function () {};
   
   $scope.nuevoContacto = function () {
 
     if ($scope.nuevo.name !== '' && $scope.nuevo.email !== '' && $scope.nuevo.phone !== '') {
-      // $scope.contactos.push(angular.copy($scope.nuevo));
-      // $scope.nuevo = { name: '', email: '', phone: '' };
       ref.push(
         {
         name: $scope.nuevo.name,
@@ -35,6 +30,7 @@ app.controller("agendaCtrl", function ($scope) {
         $scope.nuevo = { name: '', email: '', phone: '' };
         }
       );
+      //generarJson();
     }
 
   };
@@ -47,6 +43,15 @@ app.controller("agendaCtrl", function ($scope) {
         $scope.contactos.splice(index, 1);
         }, 500); // Se puede poner 0 en vez de 500 ms
     });
+    //generarJson();
   };
-
+/*generarJson = function()
+  {
+    var json =  '{"@context": "http://schema.org","@type": "WebPage","name": "AgendaJS","description": "Una gran agenda hecha en AngularJS, tomada como ejemplo del ejercicio hecho en clase.","publisher": {"@type": "Organization","name": "Universidad de Deusto", "member": [{"@type": "Person", "givenName":"Beñat", "familyName": "Galdós", "gender":"male", "email": "benatgaldos@opendeusto.es"},{"@type": "Person", "givenName":"Garikoitz", "familyName": "Bereciartua", "gender":"male", "email": "gari.bereciartua@opendeusto.es"},{"@type": "Person", "givenName":"Imanol", "familyName": "Echeverría", "gender":"male", "email": "imanol.echeverria@opendeusto.es"},{"@type": "Person", "givenName":"Anne", "familyName": "Idigoras", "gender":"female", "email": "anneidigoras@opendeusto.es"} ] }'
+      json += '"contactos":{[';
+      for (i=0;i<$scope.contactos.length;i++){
+        json += ' {"email": "'+$scope.contactos[i].email+'", "name":"'+$scope.contactos[i].name+'", "phone": "'+$scope.contactos[i].phone+'"}';
+      }
+      json+= ']}}'
+  }*/
 });
